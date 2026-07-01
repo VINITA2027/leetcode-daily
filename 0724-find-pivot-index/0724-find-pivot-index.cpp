6131bef89b32c3268f6d56dabbbdc61141e94e74
VINITA2027 <vinitatanu4141@gmail.com>
2026-07-01 21:40:14 +0530

    Time: 1 ms (26.59%), Space: 35.6 MB (96.9%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int>prefix;
+    int pivotIndex(vector<int>& nums) {
+        int total = 0;
+
+        for(int num:nums){
+            total += num;
+        }
+
+        int leftsum = 0;
+        for(int i=0;i<nums.size();i++){
+            int rightsum = total-leftsum-nums[i];
+            if(leftsum==rightsum){
+                return i;
+            }
+            leftsum += nums[i];
+        }
+        return -1;
+    }
+};
