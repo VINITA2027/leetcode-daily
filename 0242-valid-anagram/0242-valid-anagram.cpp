06734725fe0bba91d09bc3487d282fe2c4a66cad
VINITA2027 <vinitatanu4141@gmail.com>
2026-07-17 20:13:53 +0530

    Time: 3 ms (45.97%), Space: 9.9 MB (25.96%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mpp;
+
+        for(char ch:s){
+            mpp[ch]++;
+        }
+        for(char ch:t){
+            mpp[ch]--;
+        }
+        for(auto it:mpp){
+            if(it.second != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
